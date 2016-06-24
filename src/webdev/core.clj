d68dc81140404e5fae1fa51f8045a27e4a548cef
Eric Normand <ericwnormand@gmail.com>
2016-06-24 09:58:39 -0500

    Refactor: extract greet function.

--- src/webdev/core.clj
+++ src/webdev/core.clj
@@ -1,6 +1,11 @@
 (ns webdev.core
   (:require [ring.adapter.jetty :as jetty]))
 
+(defn greet [req]
+  {:status 200
+   :body "Hello, World!"
+   :headers {}})
+
 (defn -main [port]
-  (jetty/run-jetty (fn [req] {:status 200 :body "Hello, World!" :headers {}})
+  (jetty/run-jetty greet
                    {:port (Integer. port)}))
