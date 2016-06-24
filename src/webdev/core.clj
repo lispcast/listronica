d04a80433a42d2bddcc53661375aa4d2c68b9660
Eric Normand <ericwnormand@gmail.com>
2016-06-24 15:58:37 -0500

    Add /calc route and andler.

--- src/webdev/core.clj
+++ src/webdev/core.clj
@@ -25,10 +25,30 @@
      :body (str "Yo! " name "!")
      :headers {}}))
 
+(def ops
+  {"+" +
+   "-" -
+   "*" *
+   ":" /})
+
+(defn calc [req]
+  (let [a (Integer. (get-in req [:route-params :a]))
+        b (Integer. (get-in req [:route-params :b]))
+        op (get-in req [:route-params :op])
+        f (get ops op)]
+    (if f
+      {:status 200
+       :body (str (f a b))
+       :headers {}}
+      {:status 404
+       :body (str "Unknown operator: " op)
+       :headers {}})))
+
 (defroutes app
   (GET "/" [] greet)
   (GET "/goodbye" [] goodbye)
   (GET "/yo/:name" [] yo)
+  (GET "/calc/:a/:op/:b" [] calc)
 
   (GET "/about" [] about)
   (GET "/request" [] handle-dump)
