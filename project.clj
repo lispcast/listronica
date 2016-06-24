8a847086fc67da1a0bddf36d5197bc1d35b2978e
Eric Normand <ericwnormand@gmail.com>
2016-06-23 20:57:55 -0500

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject webdev "0.1.0-SNAPSHOT"
+  :description "TODO List app created for LispCast Web Development in Clojure."
+  :url "https://purelyfunctional.tv/web-dev-in-clojure"
+  :license {:name "CC0 1.0 Universal (CC0 1.0) Public Domain Dedication"
+            :url "http://creativecommons.org/publicdomain/zero/1.0/"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
