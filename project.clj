b4ed7f72e7ce3ba3c86507f79ab208f99c27faf4
Eric Normand <ericwnormand@gmail.com>
2016-06-23 21:02:09 -0500

    Add Ring dependency.

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "https://purelyfunctional.tv/web-dev-in-clojure"
   :license {:name "CC0 1.0 Universal (CC0 1.0) Public Domain Dedication"
             :url "http://creativecommons.org/publicdomain/zero/1.0/"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ring "1.5.0"]])
