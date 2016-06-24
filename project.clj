71c9d360fc52247ecf05a66dc5ca7882c23b98e3
Eric Normand <ericwnormand@gmail.com>
2016-06-23 21:11:32 -0500

    Tell lein where -main is.

--- project.clj
+++ project.clj
@@ -4,4 +4,6 @@
   :license {:name "CC0 1.0 Universal (CC0 1.0) Public Domain Dedication"
             :url "http://creativecommons.org/publicdomain/zero/1.0/"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [ring "1.5.0"]])
+                 [ring "1.5.0"]]
+
+  :main webdev.core)
