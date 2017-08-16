acb9382f7ab040f507690a8b835d48fe644b6fa5
Eric Normand <ericwnormand@gmail.com>
2017-08-16 16:31:28 -0500

    Item table

--- src/webdev/item/view.clj
+++ src/webdev/item/view.clj
@@ -11,6 +11,20 @@
           [:link {:href "/bootstrap/css/bootstrap.min.css"
                   :rel :stylesheet}]]
          [:body
-          [:div.container]
+          [:div.container
+           [:h1 "My Items"]
+           [:div.row
+            (if (seq items)
+              [:table.table.table-striped
+               [:thead
+                [:tr
+                 [:th "Name"]
+                 [:th "Description"]]]
+               [:tbody
+                (for [i items]
+                  [:tr
+                   [:td (h (:name i))]
+                   [:td (h (:description i))]])]]
+              [:div.col-sm-offset-1 "There are no items."])]]
           [:script {:src "https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"}]
           [:script {:src "/bootstrap/js/bootstrap.min.js"}]]))
